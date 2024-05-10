af3173d3e9f59463e00222bdb201e98217fd8e8e
PixHive <99159783+PixHive@users.noreply.github.com>
2024-05-10 11:01:46 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = 'xxx'
+FLOWER_GROUP_CHAT_ID = 'xxx'  # 替换为花频道的群组ID
+FRIEND_GROUP_CHAT_ID = 'xxx'  # 替换为群友问频道的群组ID
+FLOWER_AUDIT_GROUP_CHAT_ID = 'xx'  # 替换为花频道审核群组ID
+FRIEND_AUDIT_GROUP_CHAT_ID = 'xxx'  # 替换为群友问频道审核群组ID
+
+ADMIN_USER_ID = 'xxx'  # 替换为您的Telegram用户ID
+
+blacklist = set()
+appeals = {}
